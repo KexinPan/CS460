00266ff4946abe072d2bd69e57a78f072d10b0d4
KexinPan <kpan17@mail.wou.edu>
2018-10-12 12:12:30 -0700

    create QueueInterface.cs

---
+++ HW3/HW3/IQueueInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    interface IQueueInterface<T>
+    {
+        T push(T element);
+
+        T pop();
+
+        Boolean isEmpty();
+
+    }
+}
