a0ea5d758fb6b60ce671901011c7dd097c573d06
KexinPan <kpan17@mail.wou.edu>
2018-10-22 17:15:49 -0700

    unexpect mistake

--- HW4/HW4/Controllers/HomeController.cs
+++ HW4/HW4/Controllers/HomeController.cs
@@ -10,7 +10,6 @@ namespace HW4.Controllers
 {
     public class HomeController : Controller
     {
-<<<<<<< HEAD
         public ActionResult Index()
         {
             return View();
@@ -35,28 +34,6 @@ namespace HW4.Controllers
 
             /// get the type of unit that usee choose
             string unit = Request.QueryString["unit"];
-=======
-        
-        /// <summary>
-        /// mile convert can change mile to the other unit
-        /// </summary>
-        /// <returns>
-        /// result is double number and show a string when click button
-        /// </returns>
-        [HttpGet]
-        public ActionResult MileConvert()
-        {
-            /// output the result in Debug
-            Debug.WriteLine(Request.QueryString["miles"]);
-            Debug.WriteLine(Request.QueryString["unit"]);
-
-            /// get the input value of miles
-            string mileValue = Request.QueryString["miles"];
-            
-            /// get what kind of unit the user want to change
-            string unit = Request.QueryString["unit"];
-
->>>>>>> hw4-color
             /// set the result as a double value
             double value = Convert.ToDouble(mileValue);
 
@@ -82,17 +59,13 @@ namespace HW4.Controllers
                 {
                     message = value + " miles is equal to " + value * 1.609344 + " kilometers";
                 }
+                /// use View bag to pass the value from Controller to View
                 ViewBag.message = message;
                 
             }
-<<<<<<< HEAD
-=======
-
-            /// return ViewPage of this action
->>>>>>> hw4-color
             return View();
 
-            /// use View bag to pass the value from Controller to View
+            
 
         }
 
